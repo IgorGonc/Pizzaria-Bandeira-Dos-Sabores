ae534ab82599eeb3238cc69b658b489581032340
Igor Gonçalves <59848422+IgorGonc@users.noreply.github.com>
2024-05-28 17:57:59 -0300

    a

--- app.js
+++ app.js
@@ -5,12 +5,14 @@ const clienteRoutes = require('./routes/clienteRoutes')
 const produtoRoutes = require('./routes/produtoRoutes')
 const carrinhoRoutes = require('./routes/carrinhoRoutes')
 const pedidoRoutes = require('./routes/pedidoRoutes')
+const promocaoRoutes = require('./routes/promocaoRoutes')
 
 const Cliente = require('./models/Cliente')
 const Produto = require('./models/Produto')
 const Carrinho_Compra = require('./models/carrinho_compra')
 const Itens = require('./models/Itens')
 const Pedido = require('./models/pedido') // Certifique-se de importar o modelo de Pedido
+const Promocao = require('./models/Promocao')
 
 const app = express()
 const port = 3000
@@ -25,6 +27,7 @@ app.use('/api/clientes', clienteRoutes)
 app.use('/api/produtos', produtoRoutes)
 app.use('/api/carrinho', carrinhoRoutes)
 app.use('/api/pedido', pedidoRoutes)
+app.use('/api/promocao', promocaoRoutes)
 
 // Sincronizar os modelos na ordem correta
 sequelize
